07474224ff39e4db124e962ef133b578f7b02b83
JyotheendraD <jyotheendrad@gmail.com>
2021-01-16 12:01:51 +0530

    created input command fn for taking inputs

--- main.py
+++ main.py
@@ -11,3 +11,19 @@ voices = engine.getProperty('voices')  #getting all the available voice versions
 def talk(text):
     engine.say(text)
     engine.runAndWait()
+
+def input_command():
+    #engine.say("hello there! what can i do for you?")  #say function is built in our system speaks back the sentence.
+    #engine.runAndWait()
+    try:
+        with sr.Microphone as source:
+            print("Listening")
+            voice = Listener.listen(source)
+            my_command = listener.recognize_google(voice)
+            my_command = my_command.lower()
+            if 'assname' in my_command:
+                talk(my_command)
+    except:
+        my_command = "exception" #this is written because if microphone failes it prints this command.
+        print("microphone not working properly")
+    return my_command
